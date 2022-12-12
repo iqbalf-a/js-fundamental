2f681301860e274f0ada0159a8c5de9b38fd2e10
Moh. Iqbal Firman Ardiansyah <iqbalfirman.a25@gmail.com>
2022-12-12 16:55:05 +0700

    ADD looping

--- looping.js
+++ looping.js
@@ -1,9 +1,33 @@
-for (let i = 1; i < 5; i++) {
-    console.log(i);
+// for (let i = 1; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let angka = 0
+// while(angka<5) {
+//     console.log(angka)
+//     angka++
+// }
+
+const days = ["senin", 'selasa', 'rabu']
+
+days.forEach(element => {
+    console.log(element)
+});
+
+// index
+for(let dayIdx in days) {
+    console.log(days[dayIdx])
 }
 
-let angka = 0
-while(angka<5) {
-    console.log(angka)
-    angka++
+// data
+for (const iterator of days) {
+    console.log(iterator)
+}
+
+// index
+for (const key in days) {
+    if (Object.hasOwnProperty.call(days, key)) {
+        const element = days[key];
+        console.log(element)
+    }
 }
